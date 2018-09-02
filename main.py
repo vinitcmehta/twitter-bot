329f46130480123b3ebc6761dcdc559e98277f35
Vinit Mehta <vinit_m@hotmail.co.uk>
2018-09-02 14:44:19 +0100

    Hellow World!

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ for follower in tweepy.Cursor(api.followers).items():
     follower.follow()
     print ("Followed everyone that is following " + user.name)
 
-print("end")
+tweet = 'Hello, world!'
+api.update_status(status=tweet)
 
